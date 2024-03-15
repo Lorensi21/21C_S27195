5933a34ae3b95f4237b34bd0fd83fd73eebdd0e7
s27195 <vl.pochtaruk@gmail.com>
2024-03-15 16:41:35 +0100

    in task 2 I do the same, but now I use function

--- Lab3.py
+++ Lab3.py
@@ -1,3 +1,11 @@
 #task1
 squares = [x**2 for x in range(1, 11)]
 print(squares)
+#task2
+def generate_squares(start, end):
+    squares = [x**2 for x in range(start, end+1)]
+    return squares
+
+start = 1
+end = 10
+print(generate_squares(start, end))
