a1cbe3da9e02c5669de757db9927fe8fbbabf0eb
s27195 <vl.pochtaruk@gmail.com>
2024-03-15 16:43:32 +0100

    in task 3 I do the same, but now I use class + function

--- Lab3.py
+++ Lab3.py
@@ -1,6 +1,7 @@
 #task1
 squares = [x**2 for x in range(1, 11)]
 print(squares)
+
 #task2
 def generate_squares(start, end):
     squares = [x**2 for x in range(start, end+1)]
@@ -9,3 +10,13 @@ def generate_squares(start, end):
 start = 1
 end = 10
 print(generate_squares(start, end))
+
+#task3
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        squares = [x**2 for x in range(start, end+1)]
+        return squares
+
+# Example usage:
+generator = SquareGenerator()
+print(generator.generate_squares(1, 10))
